da20e70a118d21af10cebeb2a9e83c69bfcd790c
rawat0710 <ritikrawat0710@gmail.com>
2020-04-05 13:26:45 +0530

    added users route file

--- server.js
+++ server.js
@@ -23,5 +23,5 @@ app.use("*", (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log("Listening on port " + PORT + ".....");
+  console.log("Listening on port " + PORT + "...");
 });
